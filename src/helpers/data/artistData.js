0e75af82c04364d2566a00a7c1ab8a260dbd8b79
Sean <jb@JBs-MacBook-Pro.local>
2021-06-05 13:02:21 -0500

    added api call for artist

---
+++ src/helpers/data/artistData.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import songKickApi from './songKickApi';
+
+const apiKey = songKickApi;
+
+const getArtists = (artist) => new Promise((resolve, reject) => {
+  axios.get(`https://api.songkick.com/api/3.0/search/artists.json?apikey=${apiKey}&query=${artist}`)
+    .then((response) => console.warn(response.data.resultsPage.results.artist))
+    .catch((error) => reject(error));
+});
+
+export default getArtists;
--- src/views/myArtistsPage.js
+++ src/views/myArtistsPage.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Button, TextField, Typography } from '@material-ui/core';
 import HomeButton from '../components/HomeButton';
+import getArtists from '../helpers/data/artistData';
 
 const MyArtistsPage = () => {
   console.warn('my artists');
+
+  // const [artist, setArtist] = useState('');
+
+  // const handleSearch = (artist) => {
+  //   getArtists(artist);
+  // };
   return (
     <>
       <Typography>My Artists</Typography>
+      <Button onClick={() => getArtists('Incubus')}>Get Some Art</Button>
+      <form>
+        <TextField id="standard-basic" label="Standard" />
+      </form>
       <HomeButton />
     </>
   );
